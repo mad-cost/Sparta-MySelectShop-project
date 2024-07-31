79d2380fb8313d5fa8ea48a9e591cc394f88057b
mad-cost <mad_scientist1998@naver.com>
2024-07-31 14:32:21 +0900

    테스트 코드 주석

--- src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
+++ src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
@@ -1,26 +1,26 @@
-package com.sparta.myselectshop.mvc;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.io.IOException;
-
-public class MockSpringSecurityFilter implements Filter {
-  @Override
-  public void init(FilterConfig filterConfig) {}
-
-  // Security가 동작을 하면, 테스트하는데 방해가 되기 때문에 MockSecurity를 사용
-  @Override
-  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-    SecurityContextHolder.getContext()
-            .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
-    chain.doFilter(req, res);
-  }
-
-  @Override
-  public void destroy() {
-    SecurityContextHolder.clearContext();
-  }
-}
+//package com.sparta.myselectshop.mvc;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+//import java.io.IOException;
+//
+//public class MockSpringSecurityFilter implements Filter {
+//  @Override
+//  public void init(FilterConfig filterConfig) {}
+//
+//  // Security가 동작을 하면, 테스트하는데 방해가 되기 때문에 MockSecurity를 사용
+//  @Override
+//  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+//    SecurityContextHolder.getContext()
+//            .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+//    chain.doFilter(req, res);
+//  }
+//
+//  @Override
+//  public void destroy() {
+//    SecurityContextHolder.clearContext();
+//  }
+//}
